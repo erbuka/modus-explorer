bfb6dadda0f14d17f9758e29d02e1412cfebeded
Andrea Bucaletti <erbuka@hotmail.com>
2020-05-15 14:25:02 +0200

    --

--- src/app/link.directive.ts
+++ src/app/link.directive.ts
@@ -1,14 +1,21 @@
-import { Directive, Input, HostListener } from '@angular/core';
+import { Directive, Input, HostListener, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { LocationRouterService } from './location-router.service';
 
 @Directive({
   selector: '[cnLink]'
 })
-export class LinkDirective {
+export class LinkDirective implements OnInit {
 
   @Input("cnLink") link: string;
 
-  constructor(private locationRouter: LocationRouterService) { }
+  constructor(private locationRouter: LocationRouterService, private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    if (this.elementRef.nativeElement instanceof HTMLAnchorElement) {
+      this.renderer.setAttribute(this.elementRef.nativeElement, "href", this.link);
+    }
+  }
+
 
   @HostListener("click")
   onClick() {
